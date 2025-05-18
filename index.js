b056fac1154498f49e484e7a77523b40a78c924b
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-05-18 11:51:11 -0500

    add sticky navbar, update experience, add curriculum botton to navbar

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const d = document,
   n = navigator,
+  w = window,
   $listProgress = d.querySelectorAll('progress'),
   $listProgressText = d.querySelectorAll('.section-skills li'),
-  $body = d.querySelector('body')
+  $body = d.querySelector('body'),
+  $headers = document.querySelectorAll(
+    '.section-experience h2, .section-education h2, .section-skills h2'
+  )
 
 function typeValueProgress() {
   $listProgressText.forEach((p) => {
@@ -25,4 +29,18 @@ d.addEventListener('DOMContentLoaded', (e) => {
   typeValueProgress()
   animateProgress()
   $body.classList.toggle('body-hidden')
+})
+
+w.addEventListener('scroll', () => {
+  $headers.forEach((header) => {
+    const rect = header.getBoundingClientRect()
+    const isAtTopOrAbove = rect.top <= 0
+    const isVisible = rect.bottom > 0
+
+    if (isAtTopOrAbove && isVisible) {
+      header.classList.add('sticky-on')
+    } else {
+      header.classList.remove('sticky-on')
+    }
+  })
 })
