ea18625b9f29726139f56215d58b9b3f16f62927
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-09-10 17:03:26 -0500

    update icons

--- js/projects.js
+++ js/projects.js
@@ -5,11 +5,11 @@ const d = document,
 function toggleDarkMode() {
 	if (!$body.classList.contains('dark')) {
 		$body.classList.toggle('dark')
-		$darkBtn.setAttribute('src', 'assets/img/icons/light-mode.png')
+		$darkBtn.setAttribute('src', 'assets/img/icons/light.svg')
 		localStorage.setItem('dark-mode', 'enabled')
 	} else {
 		$body.classList.toggle('dark')
-		$darkBtn.setAttribute('src', 'assets/img/icons/dark-mode.svg')
+		$darkBtn.setAttribute('src', 'assets/img/icons/dark.svg')
 		localStorage.setItem('dark-mode', 'disabled')
 	}
 }
@@ -18,10 +18,10 @@ function localDarkMode() {
 	const darkMode = localStorage.getItem('dark-mode')
 	if (darkMode === 'enabled') {
 		$body.classList.add('dark')
-		$darkBtn.setAttribute('src', 'assets/img/icons/light-mode.png')
+		$darkBtn.setAttribute('src', 'assets/img/icons/light.svg')
 	} else {
 		$body.classList.remove('dark')
-		$darkBtn.setAttribute('src', 'assets/img/icons/dark-mode.svg')
+		$darkBtn.setAttribute('src', 'assets/img/icons/dark.svg')
 	}
 }
 
